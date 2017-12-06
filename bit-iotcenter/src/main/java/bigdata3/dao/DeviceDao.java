ef00ca5f3a890e7a899a3d02babcb573a1ca66d8
SUBEENKIM <subeen9313@gmail.com>
2017-12-06 21:02:13 +0900

    '.'

--- bit-iotcenter/src/main/java/bigdata3/dao/DeviceDao.java
+++ bit-iotcenter/src/main/java/bigdata3/dao/DeviceDao.java
@@ -6,5 +6,5 @@ import java.util.Map;
 import bigdata3.domain.Device;
 
 public interface DeviceDao {
-	List<Device> selectListByType(Map<String,Object> params); //타입으로 디바이스 장비 한개 찾기
+	List<Device> selectListByType(Map<String, Object> params); //타입으로 디바이스 장비 한개 찾기
 }
