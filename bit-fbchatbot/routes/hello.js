339222e3fafc48ea16314d0c8b026dd48f0c8945
SUBEENKIM <subeen9313@gmail.com>
2017-12-09 17:29:04 +0900

    '.'

--- bit-fbchatbot/routes/hello.js
+++ bit-fbchatbot/routes/hello.js
@@ -14,6 +14,7 @@ router.get('/', (request, response) => {
     })
     response.write('hello.....\n')
     response.end()
+    console.log('ㅎㅇㅎㅇ');
 })
 
 router.get('/test1', (request, response) => {
