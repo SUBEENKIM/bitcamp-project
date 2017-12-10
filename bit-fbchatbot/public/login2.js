79bf0cf24525bbd98de622ffb09cc9b7f7cb1c98
SUBEENKIM <subeen9313@gmail.com>
2017-12-10 16:01:33 +0900

    '.'

---
+++ bit-fbchatbot/public/login2.js
@@ -0,0 +1,67 @@
+<script type="text/javascript">
+ 	 window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '키입력',
+	      xfbml      : true,
+	      version    : 'v2.9'
+	    });
+	    FB.AppEvents.logPageView();
+
+	 	FB.getLoginStatus(function(response) {
+		  if (response.status === 'connected') {
+
+		  }
+		  else {
+			 FB.login(function(response) {
+			  // handle the response
+			}, {scope: 'public_profile, email, user_birthday '});
+		  }
+		});
+
+	  };
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+
+		function checkLoginState() {
+			FB.getLoginStatus(function(response) {
+			    statusChangeCallback(response);
+			  });
+		}
+ 	  function statusChangeCallback(response) {
+ 	    if (response.status === 'connected') {
+ 	      testAPI();
+ 	    } else {
+ 	      document.getElementById('status').innerHTML = 'Please log into this app.';
+ 	    }
+ 	  }
+ 	 function testAPI() {
+ 		FB.api('/me', function(response) {
+ 	      	console.log(response.id);
+ 	      	console.log(response.name);
+ 	    });
+ 		FB.api('/me/picture?type=large',function(response){
+			console.log(response.data.url);
+		});
+ 		FB.api('/me', {fields: 'email'}, function(response) {
+ 	      	console.log(response.email);
+ 	    });
+ 		FB.api('/me', {fields: 'age_range '}, function(response) {
+ 	      	console.log(response.age_range );
+ 	    });
+ 		FB.api('/me', {fields: 'gender '}, function(response) {
+ 	      	console.log(response.gender );
+ 	    });
+ 		FB.api('/me', {fields: 'locale '}, function(response) {
+ 	      	console.log(response.locale );
+ 	    });
+ 	  }
+
+</script>
+
+	<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+	</fb:login-button>
