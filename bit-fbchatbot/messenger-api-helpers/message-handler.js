0aae88551e1f98e47d3e88e1cd68d522c31b6f41
SUBEENKIM <subeen9313@gmail.com>
2017-12-11 10:41:22 +0900

    '.'

--- bit-fbchatbot/messenger-api-helpers/message-handler.js
+++ bit-fbchatbot/messenger-api-helpers/message-handler.js
@@ -64,7 +64,7 @@ addMessage('login', (recipientId) => {
           "buttons":[
             {
               "type":"web_url",
-              "url":"https://www.subeenk.xyz:9999/login.jade",
+              "url":"https://www.subeenk.xyz:9999/login3.html",
               "title":"로그인",
               "payload":"/login"
             }
