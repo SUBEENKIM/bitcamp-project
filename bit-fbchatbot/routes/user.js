18a811f4894baf4ba6e5d066b629075eae8efe90
SUBEENKIM <subeen9313@gmail.com>
2017-12-12 12:05:20 +0900

    '.'

--- bit-fbchatbot/routes/user.js
+++ bit-fbchatbot/routes/user.js
@@ -9,7 +9,47 @@ const fs = require('fs');
 // 클라이언트 요청이 들어왔을 때 함수를 호출해주는 객체
 const router = express.Router();
 
-router.get('/user', (request, response) => {
+const linkAccountToMessenger = (res, username, redirectURI) => {
+  /*
+    The auth code can be any thing you can use to uniquely identify a user.
+    Once the redirect below happens, this bot will receive an account link
+    message containing this auth code allowing us to identify the user.
+
+    NOTE: It is considered best practice to use a unique id instead of
+    something guessable like a users username so that malicious
+    users cannot spoof a link.
+   */
+  const authCode = uuid();
+
+  // set the messenger id of the user to the authCode.
+  // this will be replaced on successful account link
+  // with the users id.
+  UserStore.linkMessengerAccount(username, authCode);
+
+    // Redirect users to this URI on successful login
+  const redirectURISuccess = `${redirectURI}&authorization_code=${authCode}`;
+
+  res.redirect(redirectURISuccess);
+};
+
+router.get('/login', function(req, res) {
+  /*
+    Account Linking Token is never used in this demo, however it is
+    useful to know about this token in the context of account linking.
+
+    It can be used in a query to the Graph API to get Facebook details
+    for a user. Read More at:
+    https://developers.facebook.com/docs/messenger-platform/account-linking
+  */
+  // const accountLinkingToken = req.query.account_linking_token;
+  //
+  // const redirectURI = req.query.redirect_uri;
+  //
+  // res.render('login', {accountLinkingToken, redirectURI});
+  console.log(okok);
+});
+
+router.get('/', (request, response) => {
     console.log('okok')
 });
 
